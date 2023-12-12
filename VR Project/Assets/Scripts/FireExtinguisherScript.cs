14d6f1c09928e365512e90772177b17be03a5b22
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2023-12-12 14:15:48 +0530

    Added smoke to end of anchor

--- VR Project/Assets/Scripts/FireExtinguisherScript.cs
+++ VR Project/Assets/Scripts/FireExtinguisherScript.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class FireExtinguisherScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField]
-    private float amountExtinguishedperSecond = 0.5f;
 
-    public GameObject fireExtinguisher;
+    public bool isHeld = false;
+
+    //[SerializeField]
+    //private float amountExtinguishedperSecond = 0.5f;
+
+    //public GameObject fireExtinguisher;
 
 
     private void Update()
     {
-        if (Physics.Raycast(fireExtinguisher.transform.position, fireExtinguisher.transform.forward, out RaycastHit hit, 100f) && hit.collider.TryGetComponent(out FireScript fire))
-        {
-            fire.TryExtinguish(amountExtinguishedperSecond * Time.deltaTime);
-            Debug.Log(hit.collider.name);
-        }
+        //if (Physics.Raycast(fireExtinguisher.transform.position, fireExtinguisher.transform.forward, out RaycastHit hit, 100f) && hit.collider.TryGetComponent(out FireScript fire))
+        //{
+        //    fire.TryExtinguish(amountExtinguishedperSecond * Time.deltaTime);
+        //    Debug.Log(hit.collider.name);
+        //}
+    }
+
+    // Call this function when the fire extinguisher is picked up
+    public void Pickup()
+    {
+        isHeld = true;
+    }
+
+    // Call this function when the fire extinguisher is released
+    public void Release()
+    {
+        isHeld = false;
     }
 }
