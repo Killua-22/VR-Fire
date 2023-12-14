3b7e4ffdc9953dd23d3081d5d241b4f37e54efd9
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2023-12-15 02:53:31 +0530

    fully functional fire extinguisher
    + can extinguish the fire now.
    - disabled the collider of cylinder
    + The fire layer has name "Fire"

--- VR Project/Assets/FoamTrigger.cs
+++ VR Project/Assets/FoamTrigger.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem;
 
 public class FoamTrigger : MonoBehaviour
 {
+    //[SerializeField]
+    //private float amountExtinguishedperSecond = 0.5f;
     public GameObject LeftController;
     public GameObject RightController;
     public ParticleSystem foam;
@@ -13,10 +15,13 @@ public class FoamTrigger : MonoBehaviour
     public InputActionProperty LeftTrigger;
     public InputActionProperty RightTrigger;
     XRGrabInteractable grabbable;
+    public GameObject endAnchor;
+    private bool isFoamPlaying = false;
 
     void Start()
     {
         grabbable = GetComponent<XRGrabInteractable>();
+        
     }
 
    
@@ -27,11 +32,25 @@ public class FoamTrigger : MonoBehaviour
         if (RightTrigger.action.ReadValue<float>() > 0.1f)
         {
             foam.Play();
+            //if(Physics.Raycast(endAnchor.transform.position, endAnchor.transform.forward, out RaycastHit hit, 100f) && hit.collider.TryGetComponent(out FireScript fire))
+            //{
+            //    fire.TryExtinguish(amountExtinguishedperSecond * Time.deltaTime);
+            //    Debug.Log(hit.collider.name);
+            //}
+            isFoamPlaying = true;
         }
 
         else
         {
             foam.Stop();
+            isFoamPlaying = false;
         }
     }
+
+    public bool IsFoamPlaying()
+    {
+        return isFoamPlaying;
+    }
+
+
 }
--- VR Project/Assets/Scripts/EndAnchorScript.cs
+++ VR Project/Assets/Scripts/EndAnchorScript.cs
@@ -10,50 +10,36 @@ public class EndAnchorScript : MonoBehaviour
 
     [SerializeField]
     private float amountExtinguishedperSecond = 0.5f;
-    public GameObject extinguisherSmoke;
-    public ParticleSystem ex_smoke;
+    //public GameObject extinguisherSmoke;
+    //public ParticleSystem ex_smoke;
     public GameObject endAnchor;
-
-
-    public XRController controller;
+    public FoamTrigger foamTrigger;
 
     void Start()
     {
-        ex_smoke = extinguisherSmoke.GetComponentInChildren<ParticleSystem>();
-        if(ex_smoke.isPlaying)
-        {
-            ex_smoke.Stop();
-        }
+        
     }
 
-   
-    
 
     // Update is called once per frame
-    //void Update()
-    //{
-    //    bool isTriggerPressed = false;
-    //    if (controller.inputDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out isTriggerPressed) && isTriggerPressed)
-    //    {
-            
-    //        if (ex_smoke.isPlaying)
-    //        {
-    //            ex_smoke.Stop();
-    //        }
-    //        else
-    //        {
-    //            ex_smoke.Play();
-    //            if (Physics.Raycast(endAnchor.transform.position, endAnchor.transform.forward, out RaycastHit hit, 100f) && hit.collider.TryGetComponent(out FireScript fire))
-    //            {
-    //                fire.TryExtinguish(amountExtinguishedperSecond * Time.deltaTime);
-    //                Debug.Log(hit.collider.name);
-    //            }
-    //        }
+    void Update()
+    {
+        
+        if (foamTrigger.IsFoamPlaying())
+        {
+            int layerMask = LayerMask.GetMask("Fire");
+            if (Physics.Raycast(endAnchor.transform.position, endAnchor.transform.forward, out RaycastHit hit, 100f, layerMask))
+            {
+                Debug.Log(hit.collider.name);
+                if(hit.collider.TryGetComponent(out FireScript fire))
+                {
+                    fire.TryExtinguish(amountExtinguishedperSecond * Time.deltaTime);
+                    Debug.Log("Extinguishing");
+                }
                 
-            
-    //    }
-        
+            }
+        }
 
-        
+
     }
-
+}
