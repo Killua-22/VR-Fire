1effbac9021960495109cff02a0d12fdf180210b
Tanish-Raorane <tanishraorane4@gmail.com>
2023-12-23 14:04:15 +0530

    .

--- VR Project/Assets/DisableFoam.cs
+++ VR Project/Assets/DisableFoam.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class DisableFoam : MonoBehaviour
 {
     public GameObject Foam;
+    XRGrabInteractable interactable;
+    public InputActionProperty LeftGrab;
+    public InputActionProperty RightGrab;
+ 
     void Start()
     {
-        
+        interactable = GetComponent<XRGrabInteractable>();
     }
 
     public void OnSelect()
     {
         Foam.SetActive(true);
+       //interactable.interactionLayers = LayerMask.GetMask("Right Controller")
+
     }
     void Update()
     {
